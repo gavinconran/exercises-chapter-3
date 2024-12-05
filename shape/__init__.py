ccfd684e6daa6faa6dac55873cfa7cdb4888146b
gavinconran <gtconran@gmail.com>
2024-12-05 14:54:14 +0000

    absolute imports for shape

--- shape/__init__.py
+++ shape/__init__.py
@@ -1,5 +1,8 @@
 ## absolute imports
-#from shape.shape import Circle
+from shape.circle import Circle
+from shape import circle
+from shape import Circle
 
 ## relative import
-from .shape import Circle 
+#from .shape import Circle 
+#from .shape import circle
