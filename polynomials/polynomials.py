2275c723a6423d4bc786698897bd30e4be806d1e
gavinconran <gtconran@gmail.com>
2024-12-06 15:33:16 +0000

    chp 4 style recommendations

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -1,16 +1,26 @@
+"""This file contains the class Polynomial for Ex 3.3-5 of OOP4Maths."""
 from numbers import Number
 import numpy as np
 
 
 class Polynomial:
+    """
+    The Polynomial class reprents a polynomial function.
+
+    :param coefs: tuple containing the polynomial coefficients
+                  i-th coefficient represents the coefficient x^i  
+    """
 
     def __init__(self, coefs):
+        """Polynomial class constructor method."""
         self.coefficients = coefs
 
     def degree(self):
+        """Returns the degree of the polynomial."""
         return len(self.coefficients) - 1
 
     def __str__(self):
+        """Called by print() and returns a string representation of Polynomial."""
         coefs = self.coefficients
         terms = []
 
@@ -25,19 +35,24 @@ class Polynomial:
         return " + ".join(reversed(terms)) or "0"
 
     def __repr__(self):
+        """Called by repr() and returns a string representation of Polynomial."""
         return self.__class__.__name__ + "(" + repr(self.coefficients) + ")"
 
     def __eq__(self, other):
-
+        """Vecks if Polynomials self and other are equal."""
         return isinstance(other, Polynomial) and\
              self.coefficients == other.coefficients
 
     def __add__(self, other):
-
+        """Adds Polynomial self to Polynomial or Number other."""
         if isinstance(other, Polynomial):
+            # Work out how many coefficient places
+            # the two polynomials have in common.
             common = min(self.degree(), other.degree()) + 1
             coefs = tuple(a + b for a, b in zip(self.coefficients,
                                                 other.coefficients))
+            # Append the high degree coefficients
+            # from the higher degree summand.
             coefs += self.coefficients[common:] + other.coefficients[common:]
 
             return Polynomial(coefs)
@@ -50,10 +65,11 @@ class Polynomial:
             return NotImplemented
 
     def __radd__(self, other):
+        """Adds Polynomial other to Number self."""
         return self + other
 
     def __sub__(self, other):
-
+        """Subtracts Polynomial or Number other from Polynomial self."""
         if isinstance(other, Polynomial):
             common = min(self.degree(), other.degree()) + 1
 
@@ -73,10 +89,11 @@ class Polynomial:
             return NotImplemented
 
     def __rsub__(self, other):
+        """Subtracts Polynomial other from Number self."""
         return Polynomial(tuple(-i for i in self.coefficients)) + other
 
     def __mul__(self, other):
-
+        """Multipliess Polynomial self with Polynomial or Number other."""
         if isinstance(other, Polynomial):
             result_poly=Polynomial((0,))
             for count, value in enumerate(self.coefficients):
@@ -84,6 +101,7 @@ class Polynomial:
                 temp_tup = tuple(i for i in np.zeros(count, dtype=int))
                 result_tup = temp_tup + \
                              tuple(value * j for j in other.coefficients)
+                # build associated Polynomial
                 result_poly += Polynomial(result_tup)
 
             return result_poly
@@ -95,26 +113,29 @@ class Polynomial:
             return NotImplemented
 
     def __rmul__(self, other):
+        """Multipliess Polynomial other with Number self."""
         return self * other
 
-    def __pow__(self, other):
+    def __pow__(self, power):
+        """Raises Polynomial self to the power of other."""
         result = self
-        for i in np.arange(other - 1):
+        for i in np.arange(power - 1):
             result *= self
         return result
 
-    def __call__(self, other):
-        x = other
-        result_final = 0
+    def __call__(self, x):
+        """Returns the evaluation of a Polynomial self at a scalar value x."""
+        result = 0
         for power, value in enumerate(self.coefficients):
-            result_final += value * x ** power
-        return result_final
+            result += value * x ** power
+        return result
 
     def dx(self):
+        """Returns the derivative of a Polynomial self as a Polynomial."""
         # check for a single integer
         if self.degree() == 0:
             return Polynomial((0,))
-        # differentiate polynomial
+        # differentiate polynomial of degree 1 or higher
         result_list = []
         for power, value in enumerate(self.coefficients):
             if power != 0:  
@@ -122,6 +143,7 @@ class Polynomial:
         return Polynomial(tuple(result_list)) 
         
 def derivative(f):
+    """Function returns the derivative of a Polynomial f as a polynomial."""
     return f.dx()
 
 
--- shape/circle.py
+++ shape/circle.py
@@ -1,14 +1,21 @@
+"""This file contains the class Circle for Ex 3.6 of OOP4Maths."""
 import math
 
 
 class Circle:
+    """
+    The Circle class reprents a circle.
+
+    :param centre: two-dimensional coordinates of the centre
+    :param radius: radius of circle.
+    """
 
     def __init__(self, centre, radius):
+        """Circle class constructor method."""
         self.centre = centre
         self.radius = radius
 
     def __contains__(self, point):
+        """Call by the keyword in."""
         return math.dist(self.centre, point) < self.radius
-
-
 
